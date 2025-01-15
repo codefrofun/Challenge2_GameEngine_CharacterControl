9ba3ad9d8ea485e31800ebc6dedd2dc905075e60
Finlayson <W0506675@campus.nscc.ca>
2025-01-15 14:51:31 -0400

    Start of project, script for player movement with debug values

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -10,16 +10,19 @@ public class InputManager : MonoBehaviour, GameInput.IPlayerActions
     public GameInput gameInput;
     void Awake()
     {
+        gameInput = new GameInput();
+        gameInput.Player.Enable();
 
+        gameInput.Player.SetCallbacks(this);
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        
+        Debug.Log("Move has been activated/pressed" + context.ReadValue<Vector2>());
     }
 }
 
 public static class InputActions
 {
-    public static Action MovePlayerEvent;
+    public static Action <Vector2> MovePlayerEvent;
 }
